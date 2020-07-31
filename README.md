6d4cd740e848ac1b133a41b444ce0fe6df4c96eb
João Arthur <joaoarthurbm@gmail.com>
2020-07-31 00:39:01 -0300

    site no ar

--- README.md
+++
@@ -1,2 +0,0 @@
-# eda
-Material utilizado na disciplina de Estruturas de Dados de Computação @ UFCG.
--- deploy.sh
+++
@@ -1,8 +0,0 @@
-MESSAGE=$1
-
-rm -rf public/*
-hugo -t kiera
-cd public
-git add --all
-git commit -m "$MESSAGE"
-git push origin gh-pages
